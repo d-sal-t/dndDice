6cc6178d1afbb3d38feb987bf3fd1160821f9279
d-sal-t <85279794+d-sal-t@users.noreply.github.com>
2023-06-02 21:18:59 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -4,71 +4,76 @@
 
 using namespace std;
 
-int main() {
+int rollDice(int diceSides, int diceCount) {
     random_device rd;
     mt19937 gen(rd());
+    uniform_int_distribution<int> distribution(1, diceSides);
+
+    int totalRoll = 0;
+    cout << "Results: ";
+
+    for (int i = 0; i < diceCount; i++) {
+        int diceRoll = distribution(gen);
+        cout << diceRoll << " ";
+        totalRoll += diceRoll;
+    }
+
+    cout << endl << "Total: " << totalRoll << endl;
+    return totalRoll;
+}
+
+bool getUserChoice() {
+    char choice;
+    cout << "Roll again? (y/n): ";
+    cin >> choice;
+
+    while (choice != 'y' && choice != 'Y' && choice != 'n' && choice != 'N') {
+        cout << "Invalid input. Enter Y/N (Yes/No): ";
+        cin >> choice;
+    }
+
+    return (choice == 'y' || choice == 'Y');
+}
+
+int main() {
+    bool flag = true;
 
     cout << "Welcome to the DnD Dice Roller!" << endl;
 
-    while (true) {
+    while (flag) {
         int diceSides;
         int diceCount;
-        int totalRoll = 0;
 
-        while (true) {
+        while (flag) {
             cout << "Sides of dice: ";
             cin >> diceSides;
 
-            if (cin.fail()) {
-                cout << "Invalid input. Please enter a positive number." << endl;
+            if (cin.fail() || diceSides <= 0) {
+                cout << "Invalid input. Enter a positive number." << endl;
                 cin.clear();
                 cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            } else if (diceSides <= 0) {
-                cout << "Invalid input. Please enter a positive number." << endl;
             } else {
                 break;
             }
         }
 
-        while (true) {
+        while (flag) {
             cout << "Number of dice: ";
             cin >> diceCount;
 
-            if (cin.fail()) {
-                cout << "Invalid input. Please enter a positive number." << endl;
+            if (cin.fail() || diceCount <= 0) {
+                cout << "Invalid input. Enter a positive number." << endl;
                 cin.clear();
                 cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            } else if (diceCount <= 0) {
-                cout << "Invalid input. Please enter a positive number." << endl;
             } else {
                 break;
             }
         }
 
         cout << "Rolling " << diceCount << " dice with " << diceSides << " sides..." << endl;
-        cout << "Results: ";
+        int totalRoll = rollDice(diceSides, diceCount);
 
-        uniform_int_distribution<int> distribution(1, diceSides);
-
-        for (int i = 0; i < diceCount; i++) {
-            int diceRoll = distribution(gen);
-            cout << diceRoll << " ";
-            totalRoll += diceRoll;
-        }
-
-        cout << endl << "Total: " << totalRoll << endl;
-
-        char choice;
-        cout << "Roll again? (y/n): ";
-        cin >> choice;
-         
-        while (choice != 'y' && choice != 'Y' && choice != 'n' && choice != 'N') {
-            cout << "Invalid input. Enter Y/N (Yes/No).";
-            cin >> choice;
-        } 
-       
-
-        if (choice != 'y' && choice != 'Y') {
+        if (!getUserChoice()) {
             break;
         }
     }
